4bb6cbea7864c5ff13922063c15cf711ad485c8f
shubham kshirsagar <117492782+Shubhamkshirsagar1@users.noreply.github.com>
2022-12-23 14:12:17 +0530

    Done

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your code here
+const itemInput = document.getElementById('item-name-input');
+const priceInput = document.getElementById('item-price-input');
+const addButton = document.getElementById('add');
+const itemsTable = document.getElementById('items');
+const totalElement = document.getElementById('total');
+
+let total = 0;
+
+addButton.addEventListener('click', () => {
+    const itemName = itemInput.value;
+    const itemPrice = priceInput.value;
+
+    if (itemName.trim() === '' || itemPrice <= 0) {
+        // Invalid input, do nothing
+        return;
+    }
+
+    // Add the item to the table
+    const row = document.createElement('tr');
+    row.innerHTML = `
+    <td role="row" id="item">${itemName}</td>
+    <td role="row" id="price">${itemPrice}</td>
+  `;
+    itemsTable.appendChild(row);
+
+    // Update the total
+    total += parseInt(itemPrice, 10);
+    totalElement.innerHTML = `Total: ${total}`;
+
+    // Clear the input fields
+    itemInput.value = '';
+    priceInput.value = '';
+});
